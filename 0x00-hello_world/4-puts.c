967e952f7b24144c77f8cf69169ac68032331d15
Moses Gitonga <asterisksmoses@gmail.com>
2023-06-15 23:22:49 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
+#!/bin/bash 
 #include <stdio.h>
 /**
- * main - A program printing a line with puts functio
-n 
+ * main - Program printing a line with puts funct
+n
  * Return: 0 (Success)
  */
 int main(void)
 {
-puts("\""Programming is like building a multilingual puzzle");
+puts("\" "Programming is like building a multilingual puzzle");
 return (0);
 }
-
