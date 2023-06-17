6b508dc800af2a9a8efc0ec6718cad72ef292dcf
Moses Gitonga <asterisksmoses@gmail.com>
2023-06-17 04:24:26 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - point of entry
+ * return: 0 each time
+ */
+
+int main(void)
+{
+	char c;
+
+	char e;
+
+	c = 'a';
+	e = 'A';
+	while 
+		(c <= 'y') {
+			putchar (c);
+			c++;
+		}
+	while 
+		(e <= 'Y') {
+			putchar (e);
+			e++;
+		}
+	return (0);
+}
+
+	     
