cb09b235da1190d92595be951aac0ec29f561ab2
Moses Gitonga <asterisksmoses@gmail.com>
2023-06-17 06:14:46 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print numbers starting from 0 to 9
+ * Return - each time 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 9; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
