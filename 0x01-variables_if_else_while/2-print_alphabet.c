b3963b9971bb7c95101d2ec2457264b4e2545a61
Moses Gitonga <asterisksmoses@gmail.com>
2023-06-17 04:02:04 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - the entry point
+ * return: 0 each time 
+ */
+
+int main(void)
+{
+	char a;
+
+	a = 'c';
+	while
+		(a <= 'x') {
+			putchar(a);
+			a++;
+
+		}
+	putchar('\n');
+	return (0);
+}
