552f84614eaff76963f6a2ae3d3d13724d4324b0
Moses Gitonga <asterisksmoses@gmail.com>
2023-06-19 08:09:32 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - program that prints a line to the standard error
- * return 1 (Success)
+ * Return: 1 (Success)
  */
+
 int main(void)
 {
 fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
