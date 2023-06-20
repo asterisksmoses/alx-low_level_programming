a6e3c173f03604b714b8c6cd080ba328294b4164
Moses Gitonga <asterisksmoses@gmail.com>
2023-06-20 14:08:15 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet 10times
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char x;
+
+	int y;
+	y = 0;
+	while (y < 10)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			_putchar(x);
+		}
+		_putchar('\n');
+		y++;
+
+	}
+}
