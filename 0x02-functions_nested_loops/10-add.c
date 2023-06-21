43a6c34ec5affd1c2f168cbfd44166344c62ef57
Moses Gitonga <asterisksmoses@gmail.com>
2023-06-21 01:51:40 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @num1: first computable value
+ * @num2: second computable value
+ * Return: Ineger value
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
