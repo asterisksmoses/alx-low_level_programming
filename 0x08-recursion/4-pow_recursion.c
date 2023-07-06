0bed74849de18af99e97d4881cd846a8281197e9
Moses Gitonga <asterisksmoses@gmail.com>
2023-07-06 05:43:47 -0700

    3-factorial.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns the value of x raised
+ * to the power of y
+ * @x: the first input value
+ * @y: the second input value
+ * Return: x to the power if y
+ */
+
+int _pow_recursion(int x, int y)
+{
+
