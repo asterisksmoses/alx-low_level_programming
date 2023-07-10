aea424c0f84d910d249fbe1bd5b99881938f855d
Moses Gitonga <asterisksmoses@gmail.com>
2023-07-10 03:39:02 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - prints the numer of arguments passed to it
+ * @argc: number of arguments
+ * @agrv: an array of arguments
+ *
+ * Return: 0 each time
+ */
+
+int main(int agrc char *agrv[])
+{
+	(void) argv; /*Ignore argv*/
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
