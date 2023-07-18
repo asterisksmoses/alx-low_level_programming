67038c290ac10a28300396d7f7ba571f64fcf6f4
Moses Gitonga <asterisksmoses@gmail.com>
2023-07-18 13:07:45 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a new type describing a dog
+ * @name: input value name of the dog
+ * @age: the age of the dog input value
+ * @owner: input value defining owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+#endif
+};
