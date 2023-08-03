540c1965f2a8563b97a22c64a18bd179953610f8
root <asterisksmoses@gmail.com>
2023-08-03 11:52:53 -0700

    main.h

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - function that prints a string followed by a new line
+ * @s: A pointer to the string
+ */
+
+void _puts_recursion(char *s)
+{
+
