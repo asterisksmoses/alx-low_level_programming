9273780374604fb971c3f39aa8c7c0955d46a02e
asterisksmoses <asterisksmoses@gmail.com>
2023-10-25 16:02:08 -0700

    2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -11,18 +11,26 @@
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *node;
+	listint_t *new_node;
 
-	node = malloc(sizeof(listint_t));
-	if (node == NULL)
+	if (head == NULL)
+	{
+		return (0);
+	}
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
 	{
 		return (NULL);
 	}
+	if (*head == NULL)
+	{
+		new_node->next = NULL;
+	}
+	else
+		new_node->next = *head;
+	new_node->n = n;
+	*head = new_node;
 
-	node->n = n;
-	node->next = *head;
-
-	*head = node;
-
-	return (node);
+	return (0);
 }
