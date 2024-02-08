4a023080eed92159199e952db2528b21b09eb290
asterisksmoses <asterisksmoses@gmail.com>
2024-02-08 04:31:59 -0800

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,4 +8,25 @@
 
 void print_binary(unsigned long int n)
 {
+	unsigned long int mk = 1UL << (sizeof(unsigned long int)
+	* 8 - 1);
+	int fl = 0;
 
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	while (mk)
+	{
+		if (n & mk)
+		{
+			_putchar('1');
+			fl = 1;
+		}
+		else if (fl)
+			_putchar('0');
+
+		mk >>= 1;
+	}
+}
