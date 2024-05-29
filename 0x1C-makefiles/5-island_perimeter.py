2184f484815e406bb2494395d7bb5eb551643b5b
asterisksmoses <asterisksmoses@gmail.com>
2024-05-29 13:37:53 -0700

    5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""This is a function 'def island_perimeter(grid):' that returns the
+perimeter of the island described in grid."""
 
 def island_perimeter(grid):
     """The function that returns the perimiter of the island."""
